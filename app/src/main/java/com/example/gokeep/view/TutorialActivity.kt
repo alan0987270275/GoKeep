f6a8b1eae45ca8cb52491fb3f6f1beb63470cffc
alan0313 <alan0987270275@gmail.com>
2021-05-21 17:22:13 +0800

    Add scrollChangeListener to recyclerview for the background and title animation. Fix some UI bug.

--- app/src/main/java/com/example/gokeep/view/TutorialActivity.kt
+++ app/src/main/java/com/example/gokeep/view/TutorialActivity.kt
@@ -2,8 +2,11 @@ package com.example.gokeep.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gokeep.R
 import com.example.gokeep.databinding.ActivityTutorialBinding
 import com.example.gokeep.view.adpter.TutorialAdapter
@@ -11,11 +14,14 @@ import cz.intik.overflowindicator.SimpleSnapHelper
 
 class TutorialActivity : AppCompatActivity() {
 
+    val TAG: String = TutorialActivity::class.java.name
+
     data class TutorialViewData(
         val imageId: Int,
         val title: String,
         val subTitle: String,
-        val message: String
+        val message: String,
+        val backgroundColor: Int
     )
 
     private lateinit var data: List<TutorialViewData>
@@ -38,30 +44,32 @@ class TutorialActivity : AppCompatActivity() {
                 R.drawable.ic_circle_save_money,
                 resources.getString(R.string.tutorial_title_1),
                 resources.getString(R.string.tutorial_sub_title_1),
-                resources.getString(R.string.tutorial_message_1)
+                resources.getString(R.string.tutorial_message_1),
+                ContextCompat.getColor(this, R.color.bluePrimary)
             ),
             TutorialViewData(
                 R.drawable.ic_circle_key,
                 resources.getString(R.string.tutorial_title_2),
                 resources.getString(R.string.tutorial_sub_title_2),
-                resources.getString(R.string.tutorial_message_2)
+                resources.getString(R.string.tutorial_message_2),
+                ContextCompat.getColor(this, R.color.blueAccent)
             ),
             TutorialViewData(
                 R.drawable.ic_circle_wallet,
                 resources.getString(R.string.tutorial_title_3),
                 resources.getString(R.string.tutorial_sub_title_3),
-                resources.getString(R.string.tutorial_message_3)
+                resources.getString(R.string.tutorial_message_3),
+                ContextCompat.getColor(this, R.color.bluePrimaryLight)
             ),
             TutorialViewData(
                 R.drawable.ic_circle_growth,
                 resources.getString(R.string.tutorial_title_4),
                 resources.getString(R.string.tutorial_sub_title_4),
-                resources.getString(R.string.tutorial_message_4)
+                resources.getString(R.string.tutorial_message_4),
+                ContextCompat.getColor(this, R.color.bluePrimaryDark)
             )
         )
     }
-
-    //TODO: Setup Recycler.OnScrollListener to show background and Title Animation
 
     private fun setUpView() = with(binding) {
 
@@ -73,11 +81,25 @@ class TutorialActivity : AppCompatActivity() {
         recyclerView.layoutManager = linearLayoutManager
         recyclerView.adapter = adapter
         adapter.addAllItem(data)
-
+        setUpRecyclerOnScrollListener()
         /**
          * Attach overflowPagerIndicator to recyclerView
          */
         viewOverflowPagerIndicator.attachToRecyclerView(recyclerView)
         SimpleSnapHelper(viewOverflowPagerIndicator).attachToRecyclerView(recyclerView)
+    }
+
+    private fun setUpRecyclerOnScrollListener() = with(binding) {
+        val scrollChangeListener = object : RecyclerView.OnScrollListener() {
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                (recyclerView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()?.let {
+                    tutorialTitle.text = data[it].title
+                    tutorialContainer.setBackgroundColor(data[it].backgroundColor)
+                }
+            }
+        }
+        recyclerView.addOnScrollListener(scrollChangeListener)
     }
 }
