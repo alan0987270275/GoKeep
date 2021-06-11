b812ab511c56f2bd993b42e8317ee3a5d567fb3b
alan0313 <alan0987270275@gmail.com>
2021-06-11 21:45:17 +0800

    [Add Feture] set different background color of cardView by tag for SpendingViewHolder.

--- app/src/main/java/com/example/gokeep/view/adpter/SpendingAdapter.kt
+++ app/src/main/java/com/example/gokeep/view/adpter/SpendingAdapter.kt
@@ -11,12 +11,14 @@ import com.example.gokeep.data.localdb.entity.Spending
 import com.example.gokeep.data.model.SpendingGroupByTag
 import com.example.gokeep.data.model.getTagImageByTitle
 import com.example.gokeep.databinding.RecyclerItemSpendingBinding
+import com.example.gokeep.util.ColorHelper
 import com.example.gokeep.util.TextFormatHelper
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
-class SpendingAdapter(private val spendingList: ArrayList<SpendingGroupByTag>) : RecyclerView.Adapter<SpendingAdapter.SpendingViewHolder>() {
+class SpendingAdapter(private val spendingList: ArrayList<SpendingGroupByTag>) :
+    RecyclerView.Adapter<SpendingAdapter.SpendingViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SpendingViewHolder =
         SpendingViewHolder(
@@ -42,7 +44,8 @@ class SpendingAdapter(private val spendingList: ArrayList<SpendingGroupByTag>) :
         spendingList.addAll(list)
     }
 
-    class SpendingViewHolder(private val itemBinding: RecyclerItemSpendingBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+    class SpendingViewHolder(private val itemBinding: RecyclerItemSpendingBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
 
         fun bind(data: SpendingGroupByTag) = with(itemBinding) {
             itemView.apply {
@@ -52,15 +55,24 @@ class SpendingAdapter(private val spendingList: ArrayList<SpendingGroupByTag>) :
                 categoryTextView.text = data.tag
                 contentTextView.text = data.concatTitle
                 val spendingString = TextFormatHelper.moneyFormat(data.sumCost)
-                if(data.tag != "Income") {
-                    moneyTextView.text = "-${spendingString}"
-                    moneyTextView.setTextColor(ContextCompat.getColor(context, R.color.bluePrimaryDark))
-                } else {
-                    moneyTextView.text = spendingString
-                    moneyTextView.setTextColor(ContextCompat.getColor(context, R.color.greenSuccess))
-                }
+                moneyTextView.text =
+                    if (data.tag != "Income") "-${spendingString}" else spendingString
+                moneyTextView.setTextColor(
+                    if (data.tag != "Income")
+                        ContextCompat.getColor(context, R.color.bluePrimaryDark)
+                    else
+                        ContextCompat.getColor(context, R.color.greenSuccess)
+                )
+
                 val simpleDateFormat = SimpleDateFormat("MMM dd", Locale.US)
                 dateTextView.text = simpleDateFormat.format(data.createdTimeStamp)
+
+                cardView.setBackgroundColor(
+                    ColorHelper.getStaticColorMap(context, data.tag) ?: ContextCompat.getColor(
+                        context,
+                        R.color.white
+                    )
+                )
             }
         }
     }
